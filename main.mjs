0eade5b50799e1a08dfd0421ba34c15651f9bf78
Alexis Haldy <alexishaldy@gmail.com>
2023-10-04 15:12:02 +0200

    Improve

--- main.mjs
+++ main.mjs
@@ -1,8 +1,3 @@
-/**
- * @fileoverview Create lookup tables for cos, sin, and tan
- * @author Tryliom
- */
-
 import fs from 'fs';
 
 const points = process.argv[2] || 1000;
@@ -14,52 +9,17 @@ function pushValue(table, value)
     else table.push(value + "f");
 }
 
-/**
- * Create a lookup table for cos/sin in intervals of points
- * @param {function} mathFunc - math function to create table for
- * @returns {string[]} cosTable - lookup table for cos or sin
- */
-function createTable(mathFunc)
+const sinTable = [];
+
+for (let i = 0; i < points; i++)
 {
-    const table = [];
-
-    for (let i = 0; i < points; i++)
-    {
-        pushValue(table, mathFunc(i * step));
-    }
-
-    return table;
+    pushValue(sinTable, Math.sin(i * step));
 }
-
-/**
- * Create a lookup table for tan in intervals of points
- * @returns {string[]} tanTable - lookup table for tan
- */
-function createTanTable()
-{
-    const table = [];
-    const tanStep = Math.PI / points;
-    let offset = -Math.PI / 2 + tanStep / 2;
-
-    for (let i = 0; i < points; i++)
-    {
-        pushValue(table, Math.tan(offset + i * tanStep));
-    }
-
-    return table;
-}
-
-// Create lookup tables for cos, sin, and tan
-const cosTable = createTable(Math.cos);
-const sinTable = createTable(Math.sin);
-const tanTable = createTanTable();
 
 // Save them in a file LookupTable.h with the step size
 fs.writeFileSync('out/LookupTable.h', `#pragma once\n\n` +
     `constexpr const float step = ${step}f;\n` +
     `constexpr const size_t size = ${points};\n\n` +
-    `constexpr const float cosTable[size] = { ${cosTable.join(', ')} };\n` +
-    `constexpr const float sinTable[size] = { ${sinTable.join(', ')} };\n` +
-    `constexpr const float tanTable[size] = { ${tanTable.join(', ')} };`);
+    `constexpr const float sinTable[size] = { ${sinTable.join(', ')} };`);
 
 // How to generate: `npm start <points or leave it empty>` in the terminal
